6dd062632bb8eca55e93c9218c4054752b0c1942
samhith-3703 <mannamsamhith@gmail.com>
2025-10-24 15:26:04 +0530

    Update Body.js

--- components/Body.js
+++ components/Body.js
@@ -3,24 +3,50 @@ const Body=()=>{
     return(
         <main>
     <h2>Recommended Movies</h2>
-    <div class="movie-grid">
-    <div class="movie-card">
-        <img src="https://upload.wikimedia.org/wikipedia/en/0/0c/OG_Poster.jpg"  alt="Movie Title 1 Poster" class="movie-poster" />
-        <h3 class="movie-title">OG</h3>
-        <span class="movie-rating">⭐ 4.7/5</span>
+    <div className="movie-container">
+    <div className="movie-card">
+        <img src="https://upload.wikimedia.org/wikipedia/en/0/0c/OG_Poster.jpg"  alt="Movie Title 1 Poster" className="movie-poster" />
+        <h3 className="movie-title">OG</h3>
+        <span className="movie-rating">⭐ 4.9/5</span>
     </div>
 
-    <div class="movie-card">
-        <img src="https://m.media-amazon.com/images/M/MV5BNTU0ZjYxOWItOWViMC00YWVlLWJlMGUtZjc1YWU0NTlhY2ZhXkEyXkFqcGc@._V1_.jpg" alt="Movie Title 2 Poster" class="movie-poster" />
-        <h3 class="movie-title">Salaar</h3>
-        <span class="movie-rating">⭐ 4.0/5</span>
+    <div className="movie-card">
+        <img src="https://m.media-amazon.com/images/M/MV5BNTU0ZjYxOWItOWViMC00YWVlLWJlMGUtZjc1YWU0NTlhY2ZhXkEyXkFqcGc@._V1_.jpg" alt="Movie Title 2 Poster" className="movie-poster" />
+        <h3 className="movie-title">Salaar</h3>
+        <span className="movie-rating">⭐ 5.7/5</span>
     </div>
 
-    <div class="movie-card">
-        <img src="https://upload.wikimedia.org/wikipedia/en/d/dc/Mahesh_khaleja_poster.jpg" alt="Movie Title 3 Poster" class="movie-poster" />
-        <h3 class="movie-title">Khaleja</h3>
-        <span class="movie-rating">⭐ 4.2/5</span>
+    <div className="movie-card">
+        <img src="https://upload.wikimedia.org/wikipedia/en/d/dc/Mahesh_khaleja_poster.jpg" alt="Movie Title 3 Poster" className="movie-poster" />
+        <h3 className="movie-title">Khaleja</h3>
+        <span className="movie-rating">⭐ 5/5</span>
     </div>
+    <div className="movie-card">
+        <img src="https://m.media-amazon.com/images/M/MV5BY2VkZjk5ZjMtM2ExOS00ZDA1LTg1ZDEtYTliNGZiYTc4ZWZiXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" alt="Movie Title 4 Poster" className="movie-poster" />
+        <h3 className="movie-title">Kantara</h3>
+        <span className="movie-rating">⭐ 4.2/5</span>
+    </div>
+    <div className="movie-card">
+        <img src="https://images.news18.com/telugu/uploads/2021/11/aparichithudu.jpg" alt="Movie Title 5 Poster" className="movie-poster" />
+        <h3 className="movie-title">aparichithudu</h3>
+        <span className="movie-rating">⭐ 3.8/5</span>
+    </div>
+    <div className="movie-card">
+        <img src="https://indiaglitz-medianw.s3.amazonaws.com/telugu/home/8-vasanthalu-review-2.jpg" alt="Movie Title 6 Poster" className="movie-poster" />
+        <h3 className="movie-title">8 vasanthalu</h3>
+        <span className="movie-rating">⭐ 4.2/5</span>
+    </div>
+    <div className="movie-card">
+        <img src="https://m.media-amazon.com/images/S/pv-target-images/c93330e8ea31247d565cb78ed20369d0bd0f8ac77cc3fdcb617dd67d63c362b4.jpg" alt="Movie Title 7 Poster" className="movie-poster" />
+        <h3 className="movie-title">F1</h3>
+        <span className="movie-rating">⭐ 4.7/5</span>
+    </div>
+    <div className="movie-card">
+        <img src="https://images.plex.tv/photo?size=large-1920&scale=1&url=https%3A%2F%2Fmetadata-static.plex.tv%2Fc%2Fgracenote%2Fcd3fa96c9ff25ab3ced94df8a1f6f28d.jpg" alt="Movie Title 8 Poster" className="movie-poster" />
+        <h3 className="movie-title">Coolie</h3>
+        <span className="movie-rating">⭐ 4.6/5</span>
+    </div>
+    
 
     </div>
   </main>
@@ -29,3 +55,4 @@ const Body=()=>{
     );
 };
 export default Body;
+
